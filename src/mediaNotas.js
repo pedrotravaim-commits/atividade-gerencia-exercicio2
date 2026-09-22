063a97c21fd2737342ff0c405266919c7550a0bf
travaimp-afk <travaimp@gmail.com>
2026-09-21 21:17:56 -0400

    corrije a media

--- src/mediaNotas.js
+++ src/mediaNotas.js
@@ -1,7 +1,6 @@
 function calcularMedia(notas) {
   const soma = notas.reduce((total, nota) => total + nota, 0);
-  // BUG: deveria dividir por notas.length, está dividindo por um valor fixo
-  return soma / 3;
+  return soma / notas.length;
 }
 
 module.exports = calcularMedia;
